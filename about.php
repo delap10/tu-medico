f673200f6005fe20f26b68cd85dbf8c0ed1d40d5
Ricardo de la Parra <de.la.parra82@hotmail.com>
2017-07-16 23:29:37 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Tú, médico</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="lib/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Fresca|Lobster|Shadows+Into+Light" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="http://code.jquery.com/jquery.js"></script>
+    <script src="lib/js/bootstrap.min.js"></script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+	    <div class="navbar-header">
+	    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	    		<span class="icon-bar"></span>
+	        	<span class="icon-bar"></span>
+	        	<span class="icon-bar"></span>                        
+	      	</button>
+	      	<a class="navbar-brand" href="#myPage">Logo</a>
+	    </div>
+	    <div class="collapse navbar-collapse" id="myNavbar">
+	    	<ul class="nav navbar-nav navbar-right">
+	        	<li><a href="index.php">HOME</a></li>
+	        	<li><a href="#about">CONÓCENOS</a></li>
+	        	<li><a href="#portfolio">COMUNIDAD</a></li>
+	        	<li><a href="contacto.php">CONTÁCTANOS</a></li>
+	      	</ul>
+	    </div>
+	  </div>
+	</nav>
+
+	<div class="jumbotron text-center">
+  		<h1>Tú, médico</h1> 
+  		<p>Salud, Atención al paciente & Trabajo en equipo</p>
+	</div>
+
+	<div id="about" class="container-fluid">
+  		<div class="row">
+    		<div class="mision">
+				<img src="lib/img/mision.jpg" alt="Misión" width="800" height="400">
+				<h1>Misión</h1>
+			</div>
+  		</div>
+	</div>
+	
+	<div class="container-fluid">
+  		<div class="row">
+    		<div class="vision">
+				<img src="lib/img/vision.jpg" class="slideanim" alt="Visión" width="800" height="400" style="float: right;">
+				<h1>Visión</h1>
+			</div>
+  		</div>
+	</div>
+
+	<footer class="container-fluid text-center">
+  		<a href="#myPage" title="To Top">
+    		<span class="glyphicon glyphicon-chevron-up"></span>
+  		</a><br>
+  		<i class="fa fa-facebook-official">
+  			<a href="https://www.facebook.com/Salud-Atenci%C3%B3n-al-paciente-Trabajo-en-equipo-663834127160759/">
+  				Salud, atención al paciente & trabajo en equipo
+  			</a>
+  		</i>
+	</footer>
+
+	<script>
+		$(document).ready(function(){
+  		// Add smooth scrolling to all links in navbar + footer link
+  			$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    		// Make sure this.hash has a value before overriding default behavior
+    			if (this.hash !== "") {
+      			// Prevent default anchor click behavior
+      				event.preventDefault();
+
+			    	// Store hash
+      				var hash = this.hash;
+
+			    	// Using jQuery's animate() method to add smooth page scroll
+      				// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      				$('html, body').animate({
+        				scrollTop: $(hash).offset().top
+      				}, 900, function(){
+    					// Add hash (#) to URL when done scrolling (default click behavior)
+        				window.location.hash = hash;
+      				});
+    			} // End if
+  			});
+  
+  			$(window).scroll(function() {
+    			$(".slideanim").each(function(){
+      				var pos = $(this).offset().top;
+
+      				var winTop = $(window).scrollTop();
+        			if (pos < winTop + 600) {
+          				$(this).addClass("slide");
+        			}
+    			});
+  			});
+		})
+	</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Tú, médico</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="lib/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Fresca|Lobster|Shadows+Into+Light" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="http://code.jquery.com/jquery.js"></script>
+    <script src="lib/js/bootstrap.min.js"></script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+	    <div class="navbar-header">
+	    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	    		<span class="icon-bar"></span>
+	        	<span class="icon-bar"></span>
+	        	<span class="icon-bar"></span>                        
+	      	</button>
+	      	<a class="navbar-brand" href="#myPage">Logo</a>
+	    </div>
+	    <div class="collapse navbar-collapse" id="myNavbar">
+	    	<ul class="nav navbar-nav navbar-right">
+	        	<li><a href="#home">HOME</a></li>
+	        	<li><a href="about.php">CONÓCENOS</a></li>
+	        	<li><a href="#portfolio">COMUNIDAD</a></li>
+	        	<li><a href="contacto.php">CONTÁCTANOS</a></li>
+	      	</ul>
+	    </div>
+	  </div>
+	</nav>
+
+	<div class="jumbotron text-center">
+  		<h1>Tú, médico</h1> 
+  		<p>Salud, Atención al paciente & Trabajo en equipo</p>
+	</div>
+
+	<!-- Container (About Section) -->
+	<div id="home" class="container-fluid">
+  		<div class="row">
+    		<div class="col-sm-8">
+      			<h2>About Company Page</h2><br>
+      			<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
+      			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      			<br><button class="btn btn-default btn-lg">Get in Touch</button>
+    		</div>
+    		<div class="col-sm-4">
+      			<span class="glyphicon glyphicon-signal logo"></span>
+    		</div>
+  		</div>
+	</div>
+
+	<div class="container-fluid bg-grey">
+  		<div class="row">
+    		<div class="col-sm-4">
+      			<span class="glyphicon glyphicon-globe logo slideanim"></span>
+    		</div>
+    		<div class="col-sm-8">
+      			<h2>Our Values</h2><br>
+      			<h4><strong>MISSION:</strong> Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
+      			<p><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    		</div>
+  		</div>
+	</div>
+
+	<!-- Container (Portfolio Section) -->
+	<div id="portfolio" class="container-fluid text-center bg-grey">
+  		<div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+    	<!-- Indicators -->
+    		<ol class="carousel-indicators">
+      			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      			<li data-target="#myCarousel" data-slide-to="1"></li>
+      			<li data-target="#myCarousel" data-slide-to="2"></li>
+    		</ol>
+
+    		<!-- Wrapper for slides -->
+    		<div class="carousel-inner" role="listbox">
+      			<div class="item active">
+        			<h4>"This company is the best. I am so happy with the result!"<br><span>Michael Roe, Vice President, Comment Box</span></h4>
+      			</div>
+      			<div class="item">
+        			<h4>"One word... WOW!!"<br><span>John Doe, Salesman, Rep Inc</span></h4>
+      			</div>
+      			<div class="item">
+        			<h4>"Could I... BE any more happy with this company?"<br><span>Chandler Bing, Actor, FriendsAlot</span></h4>
+      			</div>
+    		</div>
+
+    		<!-- Left and right controls -->
+    		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      			<span class="sr-only">Previous</span>
+    		</a>
+    		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      			<span class="sr-only">Next</span>
+    		</a>
+  		</div>
+	</div>
+
+	<footer class="container-fluid text-center">
+  		<a href="#myPage" title="To Top">
+    		<span class="glyphicon glyphicon-chevron-up"></span>
+  		</a><br>
+  		<i class="fa fa-facebook-official">
+  			<a href="https://www.facebook.com/Salud-Atenci%C3%B3n-al-paciente-Trabajo-en-equipo-663834127160759/">
+  				Salud, atención al paciente & trabajo en equipo
+  			</a>
+  		</i>
+	</footer>
+
+	<script>
+		$(document).ready(function(){
+  		// Add smooth scrolling to all links in navbar + footer link
+  			$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    		// Make sure this.hash has a value before overriding default behavior
+    			if (this.hash !== "") {
+      			// Prevent default anchor click behavior
+      				event.preventDefault();
+
+			    	// Store hash
+      				var hash = this.hash;
+
+			    	// Using jQuery's animate() method to add smooth page scroll
+      				// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      				$('html, body').animate({
+        				scrollTop: $(hash).offset().top
+      				}, 900, function(){
+    					// Add hash (#) to URL when done scrolling (default click behavior)
+        				window.location.hash = hash;
+      				});
+    			} // End if
+  			});
+  
+  			$(window).scroll(function() {
+    			$(".slideanim").each(function(){
+      				var pos = $(this).offset().top;
+
+      				var winTop = $(window).scrollTop();
+        			if (pos < winTop + 600) {
+          				$(this).addClass("slide");
+        			}
+    			});
+  			});
+		})
+	</script>
+
+</body>
+</html>
